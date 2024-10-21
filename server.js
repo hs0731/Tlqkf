ebe2b1d1011ac85b4716526d92f2bbf22578edf8
hs0731 <jujo6506@naver.com>
2024-10-21 15:10:35 +0900

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.post('/set-route', (req, res) => {
     console.log(`User Location: ${userLocation}, Destination: ${destination}`);
     res.json({ message: 'Route set successfully' });
     // ESP32에 위치 정보 전송 로직 추가
+
+    const { button } = req.body;
+    console.log(button); // 버튼 눌림 메시지 출력
+    res.json({ message: '응답을 성공적으로 수신했습니다.' });
 });
 
 app.listen(port, () => {
