97eace4f6677c0b9104c622e8ea2f0da0fd4ae98
hs0731 <jujo6506@naver.com>
2024-10-21 14:50:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(bodyParser.json());
